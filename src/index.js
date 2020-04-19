c3a9c73f50bada443401b4fad3dcc94323b0c8f6
Cooomma <payton@cooomma.info>
2020-04-20 01:49:27 +0800

    remove publicURL

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import Headers from "./components/header";
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
         <Headers />
         <App />
     </BrowserRouter>,
