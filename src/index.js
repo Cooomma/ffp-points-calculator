6ad590535d984af8b373fd0192c1558c12ea48c0
Cooomma <payton@cooomma.info>
2020-04-20 01:36:17 +0800

    remove useless component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
 import App from "./App";
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import Headers from "./components/header";
 
