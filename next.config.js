a1579176b2918b43f8969e39ea951e27c788ab96
Cooomma <payton@cooomma.info>
2020-04-29 03:50:32 +0800

    change configuration

--- next.config.js
+++ next.config.js
@@ -1,19 +1,18 @@
 const webpack = require("webpack");
 const isProd = (process.env.NODE_ENV || "production") === "production";
-const assetPrefix = isProd ? "/ffp-points-calculator" : "";
 
 module.exports = {
     exportPathMap: () => ({
         "/": { page: "/" },
     }),
-    assetPrefix: assetPrefix,
-    webpack: (config) => {
-        config.plugins.push(
-            new webpack.DefinePlugin({
-                "process.env.ASSET_PREFIX": JSON.stringify(assetPrefix),
-            })
-        );
-
+    assetPrefix: isProd ? "/ffp-points-calculator" : "",
+    webpack: (config, { dev }) => {
+        config.module.rules = config.module.rules.map((rule) => {
+            if (rule.loader === "babel-loader") {
+                rule.options.cacheDirectory = false;
+            }
+            return rule;
+        });
         return config;
     },
 };
